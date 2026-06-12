00f9823d20d1ef30dadd806d6488a7d8429ead89
SubCmd <dumspiro.coding@gmail.com>
2026-06-12 13:08:51 +0900

    RAG 기초 고급

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 MODEL = "gpt-4o-mini"
 
+import chroma_db
+from chroma_db.utils import embedding_functions
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pathlib import Path
+
+# 1) 인덱싱
+chroma = chromadb.PersistentClient(path="./naive_rag_db")
